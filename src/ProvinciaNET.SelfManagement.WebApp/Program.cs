8da862a8d459eccdd660a11ffcfe83c177580034
ggarbuglia@provincianet.com.ar <ggarbuglia@provincianet.com.ar>
2023-04-15 17:00:34 -0300

    Radzen Blazor and NLog implemented on WebApp

--- src/ProvinciaNET.SelfManagement.WebApp/Program.cs
+++ src/ProvinciaNET.SelfManagement.WebApp/Program.cs
@@ -1,41 +1,85 @@
-using Microsoft.AspNetCore.Authentication.Negotiate;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
 using ProvinciaNET.SelfManagement.WebApp.Data;
+using Microsoft.AspNetCore.Authentication.Negotiate;
+using NLog.Web;
+using NLog;
+using Radzen;
+using System.Runtime.InteropServices;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
 
-// Add services to the container.
-builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
-   .AddNegotiate();
-
-builder.Services.AddAuthorization(options =>
+try
 {
-    // By default, all incoming requests will be authorized according to the default policy.
-    options.FallbackPolicy = options.DefaultPolicy;
-});
+    var builder = WebApplication.CreateBuilder(args);
+    var config = builder.Configuration;
 
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+    StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
 
-var app = builder.Build();
+    // Add NLog to DI
+    builder.Logging.ClearProviders();
+    builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+    builder.Host.UseNLog();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    // Add Authentication;
+    builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme).AddNegotiate(options =>
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            options.EnableLdap(settings =>
+            {
+                settings.Domain = config.GetValue<string>("ldap:domain");
+                settings.MachineAccountName = config.GetValue<string>("ldap:username");
+                settings.MachineAccountPassword = config.GetValue<string>("ldap:password");
+            });
+        }
+    });
+
+    // Add Authorization
+    builder.Services.AddAuthorization(options =>
+    {
+        options.FallbackPolicy = options.DefaultPolicy;
+    });
+
+    // Add Razor and Blazor Server.
+    builder.Services.AddRazorPages();
+    builder.Services.AddServerSideBlazor();
+
+    // Add Transient
+    builder.Services.AddSingleton<WeatherForecastService>();
+    builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+    builder.Services.AddScoped<DialogService>();
+    builder.Services.AddScoped<NotificationService>();
+    builder.Services.AddScoped<TooltipService>();
+    builder.Services.AddScoped<ContextMenuService>();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseRouting();
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.MapBlazorHub();
+    app.MapFallbackToPage("/_Host");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
+catch (Exception ex)
+{
+    logger.Error(ex, "Stopped program because of exception.");
+    throw;
+}
+finally
+{
+    NLog.LogManager.Shutdown();
+}
